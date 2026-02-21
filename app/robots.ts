7582685dede85e82d0e1f75538656dd9c6a8d85f
Ashutosh Gaurav <mr.gaurav9554@gmail.com>
2026-02-21 12:36:36 +0530

    feat: update robots and sitemap configurations for improved SEO and accessibility

--- app/robots.ts
+++ app/robots.ts
@@ -1,13 +1,29 @@
 import type { MetadataRoute } from "next";
 
-const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL || "https://eazika.com";
+const SITE_URL = process.env.WEBSITE_URL || "https://eazika.com";
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
         userAgent: "*",
-        allow: ["/"],
+        allow: [
+          "/",
+          "/login",
+          "/login/rider",
+          "/login/shop",
+          "/register",
+          "/shop/register",
+          "/rider/register",
+
+          "/categories",
+          "/products",
+          "/products/*",
+
+          "/contact-us",
+          "/privacy-policy",
+          "/terms-and-conditions",
+        ],
       },
     ],
     sitemap: `${SITE_URL}/sitemap.xml`,
